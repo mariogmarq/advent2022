4d429e4779b94c47f963e3843fe599a0aecda8f3
mariogmarq <mariogarciamarq@gmail.com>
2022-12-12 12:33:59 +0100

    Optimized

--- src/day12.exs
+++ src/day12.exs
@@ -5,11 +5,12 @@ defmodule Day12Part1 do
   defp get_ending_position(grid), do: get_position(grid, "E")
   defp get_a_position(grid) do
     a = "a" |> String.to_charlist() |> hd
-    maps = grid |> Enum.map(fn x -> Enum.find_index(x, a) end)
+    maps = grid |> Enum.map(fn x -> Enum.find_index(x, fn x -> x == a end) end)
     0..(length(maps) - 1) |> Enum.filter(fn x -> Enum.at(maps, x) != nil end)
     |> Enum.map(fn i ->
       for {y, ^a} <- Enum.at(grid, i) |> Enum.zip(Stream.iterate(0, fn x -> x + 1 end)), do: {i, y}
       end)
+    |> Enum.map(fn x -> hd x end)
   end
 
   defp get_position(grid, char) do
@@ -59,25 +60,35 @@ defmodule Day12Part1 do
         queue = PriorityQueue.new()
         set = MapSet.new
         solve(grid, {0, {s, 0}}, ending, queue, set)
-      end) |> min
+      end) |> Enum.min
   end
 
 
   defp solve(_, {_, {ending, steps}}, ending, _, _), do: steps
+  defp solve(_, {nil, nil}, _, _, _), do: 9_999_999 # Cannot be solved
+
   defp solve(grid, {_, {current, steps}}, ending, queue, set) do
-    IO.inspect({current, steps})
     {x, y} = current
-    set = MapSet.put(set, current)
-    tos = [{x + 1, y}, {x, y + 1}, {x - 1, y}, {x, y - 1}] |> Enum.filter(fn to -> can_move_to?(grid, current, to) end)
-    tos = tos |> Enum.filter(fn {x, y} -> x >= 0 and y >= 0 end)
-    tos = tos |> Enum.filter(fn to -> !MapSet.member?(set, to) end)
-    {ex, ey} = ending
-    queue = tos |> Enum.reduce(queue, fn {x, y}, q ->
-      current = grid |> Enum.at(elem(current, 0)) |> Enum.at(elem(current, 1))
-      to = grid |> Enum.at(x) |> Enum.at(y)
-      PriorityQueue.put(q, abs(ex - x) + abs(ex - y) + steps + 1 + (current - to), {{x, y}, steps + 1})
-     end)
-    {next, queue} = PriorityQueue.pop(queue)
+
+    {{next, queue}, set} =
+     if MapSet.member?(set, current) do
+      {PriorityQueue.pop(queue), set}
+    else
+      set = MapSet.put(set, current)
+      # Select the next positions where we can move and have not been visited
+      tos = [{x + 1, y}, {x, y + 1}, {x - 1, y}, {x, y - 1}] |> Enum.filter(fn to -> can_move_to?(grid, current, to) end)
+      tos = tos |> Enum.filter(fn {x, y} -> x >= 0 and y >= 0 end)
+      tos = tos |> Enum.filter(fn to -> !MapSet.member?(set, to) end)
+      {ex, ey} = ending
+
+      # Insert the next positions
+      queue = tos |> Enum.reduce(queue, fn {x, y}, q ->
+        PriorityQueue.put(q, abs(ex - x) + abs(ey - y) + steps + 1, {{x, y}, steps + 1})
+      end)
+
+      {PriorityQueue.pop(queue), set}
+    end
+
     solve(grid, next, ending, queue, set)
   end
 end
@@ -86,4 +97,10 @@ data
 |> String.split("\n")
 |> Enum.map(&String.to_charlist/1)
 |> Day12Part1.solve()
+|> IO.inspect()
+
+data
+|> String.split("\n")
+|> Enum.map(&String.to_charlist/1)
+|> Day12Part1.solve2()
 |> IO.inspect()
